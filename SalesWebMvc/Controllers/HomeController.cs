27dfbed87cc84ffac4fa8fa774a01a7682d73276
Karina Vieira <karinamelov@gmail.com>
2024-09-16 14:22:58 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Email"] = "karinamelov@gmail.com";
+            ViewData["Aluno"] = "Karina Vieira";
 
             return View();
         }
